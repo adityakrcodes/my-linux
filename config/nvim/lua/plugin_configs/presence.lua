b7853a30f0513b44c864073cf0c5d1d7c91c8b9b
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2023-07-22 14:39:33 +0530

    Deleted all the premade configs so that I can do it from scratch.

--- config/nvim/lua/plugin_configs/presence.lua
+++
@@ -1,6 +0,0 @@
--- Discord Presence config for NVIM
--- this includes presence plugin and it's config
-require('presence'):setup({
-	neovim_image_text = 'Neovim Bolte!!!',
-	debounce_timeout = 10,
-})
