98b84fd7ffd8bd07937549fd3b234296aa07d977
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2023-11-16 22:58:35 +0530

    ADD: Added files for the config

--- config/nvim/lua/akc/plugins.lua
+++ config/nvim/lua/akc/plugins.lua
@@ -45,5 +45,14 @@ return require('packer').startup(function(use)
     use {
         'nvim-lualine/lualine.nvim',
         requires = { 'nvim-tree/nvim-web-devicons', opt = true }
-      }
+    }
+    use {
+        'numToStr/Comment.nvim',
+        config = function()
+            require('Comment').setup()
+        end
+    }
+    use {
+        'm4xshen/autoclose.nvim'
+    }
 end)
