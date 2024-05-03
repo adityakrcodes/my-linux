58740e25d9cb3d1b3d4ff42c9cdb3b182872ea01
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2024-05-03 19:07:18 +0530

    Migration: From Packer to Lazy.nvim

--- config/nvim/lua/akc/conf/nvimtree.lua
+++
@@ -1,5 +0,0 @@
-require("nvim-tree").setup {
-    disable_netrw       = true,
-    hijack_netrw        = true,
-    
-}
--- config/nvim/lua/akc/conf/presence.lua
+++
@@ -1,7 +0,0 @@
-require('presence'):setup({
-    neovim_image_text   = "Neovim bolte!!!",
-    debounce_timeout    = 10,
-    main_image          = "file",
-    enable_line_number  = true,
-    workspace_text      = "Working on %s"
-})
