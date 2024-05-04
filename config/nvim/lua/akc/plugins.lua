7a7fda930b395de4285d4e33e7492cf2c9c241b5
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2024-05-05 00:04:37 +0530

    ADD: Added markdown plugin

--- config/nvim/lua/akc/plugins.lua
+++ config/nvim/lua/akc/plugins.lua
@@ -59,7 +59,16 @@ local plugins = {
             },
         },
         lazy = false,
-}
+    },
+    {
+    "iamcco/markdown-preview.nvim",
+        cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+        build = "cd app && yarn install",
+        init = function()
+            vim.g.mkdp_filetypes = { "markdown" }
+        end,
+        ft = { "markdown" },
+   },
 }
 
 require("lazy").setup(plugins, opts)
