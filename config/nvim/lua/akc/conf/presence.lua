95f8ccecb7c75167e95a467f7b24798a6f04a204
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2024-11-01 19:29:23 +0530

    REMOVED: All the nvim config

--- config/nvim/lua/akc/conf/presence.lua
+++
@@ -1,8 +0,0 @@
-require("presence").setup{
-    neovim_image_text   = "Neovim BOLTE!!!",
-    debounce_timeout    = 10,
-    main_image          = "file",
-    enable_line_number  = true,
-    workspace_text      = "Working on %s"
-}
-
