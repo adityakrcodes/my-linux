077addf5835a600c5b4a072e272f442490fa64ac
Aditya Kumar <104731745+adityakrcodes@users.noreply.github.com>
2025-05-21 20:16:44 +0530

    add: added zsh custom akc config

---
+++ config/.akc-zsh.sh
@@ -0,0 +1,23 @@
+# LINUX ONLY
+# Config for zsh
+# Written by AdityaKrCodes
+
+# Aliases
+alias zshconfig="nvim ~/.zshrc"
+alias hyprconfig="nvim ~/.config/hypr"
+alias linuxconfig="nvim ~/repos/my-linux"
+alias :q="exit"
+alias q="exit"
+alias nrd="npm run dev"
+alias nrb="npm run build"
+alias code="code . && exit"
+alias sf="spf"
+alias cls="clear"
+alias activate="source ./env/bin/activate"
+alias lg="lazygit"
+alias yay="paru"
+
+# Programs
+# neofetch --ascii_distro Arch_small
+# fastfetch -l small
+pokeget random --hide-name | fastfetch --file-raw -
